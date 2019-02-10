103b67e57365a1fbe7028bc71b7c504068599ba9
williansjaime <williansjaime@Outlook.com>
2019-02-10 13:23:33 -0200

    Projeto de PH
    Nesse projeto o sistema digita o PH e ele retorna as plantas que suporta aquele PH

---
+++ ProjetoPH20182/src/ModelPH/ModelPH.java
@@ -0,0 +1,20 @@
+/*
+ * Willians 02/09/2018
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ModelPH;
+
+
+/**
+ *
+ * @author W
+ */
+public class ModelPH {
+    //private NomePlanta;
+    //private PHdaPlnata;
+        
+    
+}
---
+++ ProjetoPH20182/src/ViewPH/ViewPH.java
@@ -0,0 +1,17 @@
+/*
+ *Willians 02/09/2018
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ViewPH;
+
+
+
+/**
+ *
+ * @author W
+ */
+public class ViewPH {
+    
+}
